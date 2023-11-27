f6c1b69c992da8e09b5823532bec1a6cd51cd8f3
pakoyu <pwl@online.ua>
2023-11-27 16:11:13 +0200

    mod Task02 lesson02

--- src/com/javabasic/lesson02/homework/Task02.java
+++ src/com/javabasic/lesson02/homework/Task02.java
@@ -6,8 +6,8 @@ public class Task02 {
 		String latitude = "50°03′57″ N";
 		String longitude = "35°09′45″ Е";
 		System.out.println("Geographical coordinates Krasnokutsk:");
-		System.out.println("longitude: " + latitude);
-		System.out.println("latitude:  " + longitude);
+		System.out.println("latitude: " + latitude);
+		System.out.println("longitude:" + longitude);
 	}
 
 }
