673bc800c8f4e375a0e1b634e7396de3abf2101a
pakoyu <pwl@online.ua>
2023-11-28 13:13:24 +0200

    homework3 task1

--- src/com/javabasic/lesson03/homework/Task01.java
+++ src/com/javabasic/lesson03/homework/Task01.java
@@ -1,7 +1,18 @@
 package com.javabasic.lesson03.homework;
 
 public class Task01 {
-	public static void main(String[] args) {
-		System.out.println();
-	}
+    public static void main(String[] args) {
+        double pi = Math.PI;
+        double radiusFirstSnowGlobe = 1.0;
+        double radiusSecondSnowGlobe = 0.5;
+        double radiusThirdSnowGlobe = 0.2;
+        double snowDensity = 0.7;
+
+        double massFirstSnowGlobe = snowDensity * 4 / (3 * pi * Math.pow(radiusFirstSnowGlobe, 3));
+        double massSecondSnowGlobe = snowDensity * 4 / (3 * pi * Math.pow(radiusSecondSnowGlobe, 3));
+        double massThirdSnowGlobe = snowDensity * 4 / (3 * pi * Math.pow(radiusThirdSnowGlobe, 3));
+        double weightSnowman = massFirstSnowGlobe + massSecondSnowGlobe + massThirdSnowGlobe;
+
+        System.out.println("Weight the Snowman: " + weightSnowman);
+    }
 }
