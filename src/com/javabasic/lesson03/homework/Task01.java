3a636851ef0ea520e6473bfc536aead6bb5bdb36
pakoyu <pwl@online.ua>
2023-11-29 08:00:42 +0200

    homework3

--- src/com/javabasic/lesson03/homework/Task01.java
+++ src/com/javabasic/lesson03/homework/Task01.java
@@ -3,16 +3,16 @@ package com.javabasic.lesson03.homework;
 public class Task01 {
     public static void main(String[] args) {
         double pi = Math.PI;
-        double radiusFirstSnowGlobe = 1.0;
-        double radiusSecondSnowGlobe = 0.5;
-        double radiusThirdSnowGlobe = 0.2;
-        double snowDensity = 70;
+        float radiusFirstSnowGlobe = 1.0f;
+        float radiusSecondSnowGlobe = 0.5f;
+        float radiusThirdSnowGlobe = 0.2f;
+        float snowDensity = 0.7f;
 
         double massFirstSnowGlobe = snowDensity * ((double) 4 / 3 * pi * Math.pow(radiusFirstSnowGlobe, 3));
         double massSecondSnowGlobe = snowDensity * ((double) 4 / 3 * pi * Math.pow(radiusSecondSnowGlobe, 3));
         double massThirdSnowGlobe = snowDensity * ((double) 4 / 3 * pi * Math.pow(radiusThirdSnowGlobe, 3));
         double weightSnowman = massFirstSnowGlobe + massSecondSnowGlobe + massThirdSnowGlobe;
 
-        System.out.println("Weight the Snowman: " + weightSnowman + " kg.");
+        System.out.println("Weight the Snowman: " + weightSnowman + " kg?");
     }
 }
--- src/com/javabasic/lesson03/homework/Task02.java
+++ src/com/javabasic/lesson03/homework/Task02.java
@@ -3,8 +3,8 @@ package com.javabasic.lesson03.homework;
 public class Task02 {
     public static void main(String[] args) {
         float a = -4.2F;
-        float b = 42.0F;
-        float c = 42.42F;
+        float b = 42.42F;
+        float c = 42.0F;
 
         // | a- b | / (a + b) ^3 - √c
         double result  = Math.abs(a-b) / Math.pow((a+b), 3) - Math.sqrt(c);
