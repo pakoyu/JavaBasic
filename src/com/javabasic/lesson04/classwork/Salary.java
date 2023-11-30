08fab2797bf3a8b88e8d63cc6cce0d0ebff65a85
pakoyu <pwl@online.ua>
2023-11-30 08:17:57 +0200

    lesson04 classwork salary task

--- src/com/javabasic/lesson04/classwork/Salary.java
+++ src/com/javabasic/lesson04/classwork/Salary.java
@@ -2,6 +2,9 @@ package com.javabasic.lesson04.classwork;
 
 public class Salary {
     public static void main(String[] args) {
+        int years = 10;
+        int month = 12;
+        float tax = 5.0f;
         float salaryEmployeeFirst = 1500.00f;
         float salaryEmployeeSecond = 1000.10f;
         float salaryEmployeeThird = 999.99f;
@@ -10,28 +13,26 @@ public class Salary {
         System.out.println(salaryEmployeeSecond);
         System.out.println(salaryEmployeeThird);
 
-        float TenYearsSalaryEmployeeFirst = salaryEmployeeFirst * 10 * 12;
-        float TenYearsSalaryEmployeeSecond = salaryEmployeeSecond * 10 * 12;
-        float TenYearsSalaryEmployeeThird = salaryEmployeeThird * 10 * 12;
+        float TenYearsSalaryEmployeeFirst = salaryEmployeeFirst * years * month;
+        float TenYearsSalaryEmployeeSecond = salaryEmployeeSecond * years * month;
+        float TenYearsSalaryEmployeeThird = salaryEmployeeThird * years * month;
 
-        System.out.println(TenYearsSalaryEmployeeFirst );
+        System.out.println(TenYearsSalaryEmployeeFirst);
         System.out.println(TenYearsSalaryEmployeeSecond);
-        System.out.println(TenYearsSalaryEmployeeThird );
+        System.out.println(TenYearsSalaryEmployeeThird);
 
-        float netIncomeTenYearsEmployeeFirst = TenYearsSalaryEmployeeFirst * 0.95f;
-        float netIncomeTenYearsEmployeeSecond = TenYearsSalaryEmployeeSecond * 0.95f;
-        float netIncomeTenYearsEmployeeThird = TenYearsSalaryEmployeeThird * 0.95f;
+        float netSalaryTenYearsEmployeeFirst = TenYearsSalaryEmployeeFirst * tax / 100;
+        float netSalaryTenYearsEmployeeSecond = TenYearsSalaryEmployeeSecond * tax / 100;
+        float netSalaryTenYearsEmployeeThird = TenYearsSalaryEmployeeThird * tax / 100;
 
-        System.out.println(netIncomeTenYearsEmployeeFirst );
-        System.out.println(netIncomeTenYearsEmployeeSecond);
-        System.out.println(netIncomeTenYearsEmployeeThird );
+        System.out.println(netSalaryTenYearsEmployeeFirst);
+        System.out.println(netSalaryTenYearsEmployeeSecond);
+        System.out.println(netSalaryTenYearsEmployeeThird);
 
-        float averageSalaryEmployeeFirst = netIncomeTenYearsEmployeeFirst / 10 / 12;
-        float averageSalaryEmployeeSecond = netIncomeTenYearsEmployeeSecond / 10 / 12;
-        float averageSalaryEmployeeThird = netIncomeTenYearsEmployeeThird / 10 / 12;
+        double averageSalary = (netSalaryTenYearsEmployeeFirst +
+                netSalaryTenYearsEmployeeSecond +
+                netSalaryTenYearsEmployeeThird) / 3;
 
-        System.out.println(averageSalaryEmployeeFirst );
-        System.out.println(averageSalaryEmployeeSecond);
-        System.out.println(averageSalaryEmployeeThird );
+        System.out.println(averageSalary);
     }
 }
