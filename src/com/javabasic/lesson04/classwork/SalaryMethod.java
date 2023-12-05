a5bcc51c315b5287dab20452bc59363c595cc39a
pakoyu <pwl@online.ua>
2023-12-05 20:40:12 +0200

    lesson04 classwork

--- src/com/javabasic/lesson04/classwork/SalaryMethod.java
+++ src/com/javabasic/lesson04/classwork/SalaryMethod.java
@@ -2,22 +2,23 @@ package com.javabasic.lesson04.classwork;
 
 public class SalaryMethod {
     public static void main(String[] args) {
+        int years = 10;
         float tax = 5.0f;
         float salaryEmployeeFirst = 3000.00f;
         float salaryEmployeeSecond = 4500.00f;
         float salaryEmployeeThird = 5800.00f;
 
-        float tenYearsSalaryEmployeeFirst =  TenYearsSalary(salaryEmployeeFirst);
-        float tenYearsSalaryEmployeeSecond =  TenYearsSalary(salaryEmployeeSecond);
-        float tenYearsSalaryEmployeeThird =  TenYearsSalary(salaryEmployeeThird);
+        float tenYearsSalaryEmployeeFirst =  totalEarnings(salaryEmployeeFirst, years);
+        float tenYearsSalaryEmployeeSecond =  totalEarnings(salaryEmployeeSecond, years);
+        float tenYearsSalaryEmployeeThird =  totalEarnings(salaryEmployeeThird, years);
 
         System.out.println("Ten Years Salary Employee First - " + tenYearsSalaryEmployeeFirst);
         System.out.println("Ten Years Salary Employee Second - " + tenYearsSalaryEmployeeSecond);
         System.out.println("Ten Years Salary Employee Third - " + tenYearsSalaryEmployeeThird);
 
-        float netTenYearsSalaryEmployeeFirst =  TenYearsSalary(salaryEmployeeFirst) - calcTax(tax,tenYearsSalaryEmployeeFirst);
-        float netTenYearsSalaryEmployeeSecond =  TenYearsSalary(salaryEmployeeSecond) - calcTax(tax,tenYearsSalaryEmployeeSecond);
-        float netTenYearsSalaryEmployeeThird =  TenYearsSalary(salaryEmployeeThird) - calcTax(tax,tenYearsSalaryEmployeeThird);
+        float netTenYearsSalaryEmployeeFirst =  totalEarnings(salaryEmployeeFirst, years) - calcTax(tax,tenYearsSalaryEmployeeFirst);
+        float netTenYearsSalaryEmployeeSecond =  totalEarnings(salaryEmployeeSecond, years) - calcTax(tax,tenYearsSalaryEmployeeSecond);
+        float netTenYearsSalaryEmployeeThird =  totalEarnings(salaryEmployeeThird, years) - calcTax(tax,tenYearsSalaryEmployeeThird);
 
         System.out.println("Net Ten Years Salary Employee First - " + netTenYearsSalaryEmployeeFirst);
         System.out.println("Net Ten Years Salary Employee Second - " + netTenYearsSalaryEmployeeSecond);
@@ -30,8 +31,7 @@ public class SalaryMethod {
     public static float calcTax(float tax, float salary) {
         return salary * tax / 100;
     }
-    public static float TenYearsSalary (float salary){
-        int years = 10;
+    public static float totalEarnings (float salary, int years){
         int monthInYear = 12;
         return salary * years * monthInYear;
     }
